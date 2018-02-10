9fa6508744aa4c80672c48d6a7df73398befde2a
张广森 <sp6688@vip.qq.com>
2018-02-10 17:13:33 +0800

    后台模板页面

--- js/select.jquery.js
+++ js/select.jquery.js
@@ -1,37 +1,56 @@
 layui.define(["element","jquery"],function(exports){
     var $ = layui.jquery;
-    exports("select_Category", function(obj) {
-        return new select_Category(obj).InitData();
+    exports("select_Category", function(obj,param,callback) {
+        return new select_Category(obj,param,callback).InitData();
     });
-    function select_Category(obj) {
+    function select_Category(obj,param,callback) {
         this.Category1ID = obj.Category1ID,
         this.Category2ID = obj.Category2ID,
         this.Category3ID = obj.Category3ID,
         this.Category4ID = obj.Category4ID,
+        this.state = obj.state,
         this.DataURL = obj.DataURL, //数据URL
+        this.isClick=false;
+        this.initial=1;
+        this.paramVal={};
         // this.InitData();
         
         this.InitData = function(){
             var That=this;
             $.get(this.DataURL, {}, function(category1) {
-                var firstcategory1Guid = category1[0].Value;
                 //初始化一级分类
                 var _category=[];
-                for (var i = 0; i < category1.length; i++) {
+                for (var i = 0; i < category1.data.length; i++) {
+                    if(category1.status!=1 || category1.data.length<=0){
+                        getErrorClearElem(That.Category1ID);
+                        getErrorClearElem(That.Category2ID);
+                        getErrorClearElem(That.Category3ID);
+                        getErrorClearElem(That.Category4ID);
+                        window.top.open_layer('没有获取到赛事信息',{'icon':5});
+                        return false;
+                    }
                     var _class="";
                     if(i==0){
                         _class="class='layui-this'";
-                        // $('.j_category1 .layui-input').val(category1[i].Display);
                     }
-                    _category.push(" <dd lay-value='" + category1[i].Value + "' "+_class+">" + category1[i].Display + "</dd>");
+                    if(param && param['activityId']==category1.data[i].id && callback){
+                        That.paramVal['val1']=category1.data[i].name
+                    }
+                    _category.push(" <dd "+_class+" lay-value='" + category1.data[i].id + "' "+_class+">" + category1.data[i].name + "</dd>");
                 }
                 siblingDD(That.Category1ID).append(_category.join(''));
-                loadC2C3C4(That,firstcategory1Guid)
+                if(That.state=='edit'){
+                    loadC2C3C4(That,category1.data[0].id)
+                }else{
+                    createInit(That.Category2ID,"请选择赛制")
+                    createInit(That.Category3ID,"请选择分组")
+                    createInit(That.Category4ID,"请选择比赛")
+                }
             }, "json")
             this.inputEvent();
-            this.category1Select(true);
-            this.category2Select(true);
-            this.category3Select(true);
+            this.category1Select();
+            this.category2Select();
+            this.category3Select();
             this.category4Select();
         },
         this.inputEvent = function(){
@@ -44,17 +63,18 @@ layui.define(["element","jquery"],function(exports){
                 selectHide()
             })
         },
-        this.category1Select = function (useEmpty) {
+        this.category1Select = function () {
             var That=this;
             var oldStyle;
             $('.'+this.Category1ID).on('click','dd',function(ev){
                 ev.stopPropagation();
-                var optionHtml = "";
-                if (useEmpty) {
-                    optionHtml = '<dd lay-value="" class="layui-select-tips">请选择</dd>';
+                if(!$(this).attr('lay-value') || $(this).attr('lay-value')==""){
+                    window.top.open_layer('赛事不能为空',{'icon':5,'anim':6});
+                    return false;
                 }
-                siblingDD(That.Category2ID).html(optionHtml)
-                siblingDD(That.Category3ID).html(optionHtml)
+                That.isClick=true;
+                ++That.initial;
+                createInit(That.Category2ID,"请选择赛制")
 
                 oldStyle ? $(oldStyle).removeClass('layui-this') : $(this).siblings('.layui-this').removeClass('layui-this');
                 $(this).addClass('layui-this');
@@ -63,21 +83,21 @@ layui.define(["element","jquery"],function(exports){
                 oldStyle=this;
                 selectHide();
                 //初始化二级分类
-                $.get(That.DataURL, { pid: tmpSelectedcategory1 }, function (category2) {
-                    loadC2C3C4(That,category2[0].Value)
-                }, "json");
+                param=null;
+                loadC2C3C4(That,tmpSelectedcategory1)
             });
         },
-        this.category2Select = function (useEmpty) {
+        this.category2Select = function () {
             var That=this;
             var oldStyle;
             $('.'+this.Category2ID).on('click','dd',function(ev){
                 ev.stopPropagation();
-                var optionHtml = "";
-                if (useEmpty) {
-                    optionHtml = '<dd lay-value="" class="layui-select-tips">请选择</dd>';
+                if(!$(this).attr('lay-value') || $(this).attr('lay-value')==""){
+                    window.top.open_layer('赛制不能为空',{'icon':5,'anim':6});
+                    return false;
                 }
-                siblingDD(That.Category3ID).html(optionHtml)
+                ++That.initial;
+                createInit(That.Category3ID,"请选择分组")
 
                 oldStyle ? $(oldStyle).removeClass('layui-this') : $(this).siblings('.layui-this').removeClass('layui-this');
                 $(this).addClass('layui-this');
@@ -85,34 +105,22 @@ layui.define(["element","jquery"],function(exports){
                 $('.'+That.Category2ID+' .layui-input').val($(this).text());
                 oldStyle=this;
                 selectHide();
-                //初始化二级分类
-                $.get(That.DataURL, { pid: tmpSelectedcategory2 }, function (category3) {
-                    var tmpSelectedcategory3 = category3[0].Value;
-                    var _category3=[];
-                    for (var i = 0; i < category3.length; i++) {
-                        _category3.push(" <dd lay-value='" + category3[i].Value + "'>" + category3[i].Display + "</dd>");
-                    }
-                    siblingDD(That.Category3ID).append(_category3.join(''));
-                    $.get(That.DataURL, { pid: tmpSelectedcategory3 }, function (category4) {
-                        var _category4=[];
-                        for (var i = 0; i < category4.length; i++) {
-                            _category4.push(" <dd lay-value='" + category4[i].Value + "'>" + category4[i].Display + "</dd>");
-                        }
-                        siblingDD(That.Category4ID).append(_category4.join(''));
-                    }, "json");
-                }, "json");
+
+                param=null;
+                loadC3(That,tmpSelectedcategory2)
             });
         },
-        this.category3Select = function (useEmpty) {
+        this.category3Select = function () {
             var That=this;
             var oldStyle;
             $('.'+this.Category3ID).on('click','dd',function(ev){
                 ev.stopPropagation();
-                var optionHtml = "";
-                if (useEmpty) {
-                    optionHtml = '<dd lay-value="" class="layui-select-tips">请选择</dd>';
+                if(!$(this).attr('lay-value') || $(this).attr('lay-value')==""){
+                    window.top.open_layer('分组不能为空',{'icon':5,'anim':6});
+                    return false;
                 }
-                siblingDD(That.Category3ID).html(optionHtml)
+                ++That.initial;
+                createInit(That.Category4ID,"请选择比赛")
 
                 oldStyle ? $(oldStyle).removeClass('layui-this') : $(this).siblings('.layui-this').removeClass('layui-this');
                 $(this).addClass('layui-this');
@@ -120,15 +128,9 @@ layui.define(["element","jquery"],function(exports){
                 $('.'+That.Category3ID+' .layui-input').val($(this).text());
                 oldStyle=this;
                 selectHide();
-                //初始化二级分类
-                
-                $.get(That.DataURL, { pid: tmpSelectedcategory3 }, function (category4) {
-                    var _category4=[];
-                    for (var i = 0; i < category4.length; i++) {
-                        _category4.push(" <dd lay-value='" + category4[i].Value + "'>" + category4[i].Display + "</dd>");
-                    }
-                    siblingDD(That.Category4ID).append(_category4.join(''));
-                }, "json");
+
+                param=null;
+                loadC4(That,tmpSelectedcategory3)
             });
         },
         this.category4Select = function () {
@@ -136,52 +138,116 @@ layui.define(["element","jquery"],function(exports){
             var oldStyle;
             $('.'+this.Category4ID).on('click','dd',function(ev){
                 ev.stopPropagation();
-
+                if(!$(this).attr('lay-value') || $(this).attr('lay-value')==""){
+                    window.top.open_layer('比赛不能为空',{'icon':5,'anim':6});
+                    return false;
+                }
                 oldStyle ? $(oldStyle).removeClass('layui-this') : $(this).siblings('.layui-this').removeClass('layui-this');
                 $(this).addClass('layui-this');
                 $('.'+That.Category4ID+' .layui-input').val($(this).text());
+                setHidden($(this).attr('lay-value'))
                 oldStyle=this;
+                param=null;
                 selectHide();
             });
         }
-
+        function createInit(id,val){
+            var optionHtml = "";
+            optionHtml = '<dd lay-value="" class="layui-select-tips">'+val+'</dd>';
+            siblingDD(id).html(optionHtml)
+        }
         function selectHide(){
             $('.layui-form-select').removeClass('layui-form-selected');
         }
-        function loadC2C3C4(That,firstcategory1Guid){
+        function loadC2C3C4(That,activityId){
             //初始化二级分类
             $.get(That.DataURL, {
-                pid: firstcategory1Guid
+                activityId: param ? param['activityId'] : activityId
             }, function(category2) {
-                var firstcategory2Guid = category2[0].Value;
+                if(category2.status!=1 || category2.data.length<=0){
+                    getErrorClearElem(That.Category2ID);
+                    getErrorClearElem(That.Category3ID);
+                    getErrorClearElem(That.Category4ID);
+                    window.top.open_layer('没有获取到赛制信息',{'icon':5,'anim':6});
+                    return false;
+                }
                 var _category2=[];
-                for (var i = 0; i < category2.length; i++) {
-                    _category2.push(" <dd lay-value='" + category2[i].Value + "'>" + category2[i].Display + "</dd>");
+                That.initial>1 && $('.'+That.Category2ID+' .layui-input').val(category2.data[0].name);
+                for (var i = 0; i < category2.data.length; i++) {
+                    var _class='';
+                    if(param && param['eventId']==category2.data[i].id && callback){
+                        _class="class='layui-this'";
+                        That.paramVal['val2']=category2.data[i].name
+                    }
+                    _category2.push(" <dd "+_class+" lay-value='" + category2.data[i].id + "'>" + category2.data[i].name + "</dd>");
                 }
                 siblingDD(That.Category2ID).append(_category2.join(''));
-
-                $.get(That.DataURL, {
-                    pid: firstcategory2Guid
-                }, function(category3) {
-                    var firstcategory3Guid = category3[0].Value;
-                    var _category3=[];
-                    for (var i = 0; i < category3.length; i++) {
-                        _category3.push(" <dd lay-value='" + category3[i].Value + "'>" + category3[i].Display + "</dd>");
-                    }
-                    siblingDD(That.Category3ID).append(_category3.join(''));
-                    $.get(That.DataURL, {
-                        pid: firstcategory3Guid
-                    }, function(category4) {
-                        var _category4=[];
-                        for (var i = 0; i < category4.length; i++) {
-                            _category4.push(" <dd lay-value='" + category4[i].Value + "'>" + category4[i].Display + "</dd>");
-                        }
-                        siblingDD(That.Category4ID).append(_category4.join(''));
-
-                    }, "json");
-                }, "json");
-
+                loadC3(That,category2.data[0].id)
             }, "json");
+        }
+        function loadC3(That,eventId){
+            $.get(That.DataURL, {
+                eventId: param ? param['eventId'] : eventId
+            }, function(category3) {
+                if(category3.status!=1 || category3.data.length<=0){
+                    getErrorClearElem(That.Category3ID);
+                    getErrorClearElem(That.Category4ID);
+                    window.top.open_layer('没有获取到分组信息',{'icon':5,'anim':6});
+                    return false;
+                }
+                var _category3=[];
+                That.initial>1 && $('.'+That.Category3ID+' .layui-input').val(category3.data[0].name);
+                for (var i = 0; i < category3.data.length; i++) {
+                    var _class='';
+                    if(param && param['groupId']==category3.data[i].id && callback){
+                        _class="class='layui-this'";
+                        That.paramVal['val3']=category3.data[i].name
+                    }
+                    _category3.push(" <dd "+_class+" lay-value='" + category3.data[i].id + "'>" + category3.data[i].name + "</dd>");
+                }
+                siblingDD(That.Category3ID).append(_category3.join(''));
+                loadC4(That,category3.data[0].id)
+            }, "json");
+        }
+        function loadC4(That,groupId){
+            $.get(That.DataURL, {
+                groupId: param ? param['groupId'] : groupId
+            }, function(category4) {
+                if(category4.status!=1 || category4.data.length<=0){
+                    getErrorClearElem(That.Category4ID);
+                    window.top.open_layer('没有获取到比赛信息',{'icon':5,'anim':6});
+                    return false;
+                }
+                var _category4=[];
+                That.initial>1 && $('.'+That.Category4ID+' .layui-input').val(category4.data[0].name);
+                for (var i = 0; i < category4.data.length; i++) {
+                    var _class='';
+                    if(param && param['id']==category4.data[i].id && callback){
+                        That.paramVal['val4']=category4.data[i].name
+                        _class="class='layui-this'";
+                        callback(That.paramVal)
+                    }
+                    _category4.push(" <dd "+_class+" lay-value='" + category4.data[i].id + "'>" + category4.data[i].name + "</dd>");
+                }
+                siblingDD(That.Category4ID).append(_category4.join(''));
+                if(That.isClick){
+                    setHidden(category4.data[0].id)
+                }
+            }, "json");
+        }
+        function getErrorClearElem(cid){
+            var parent=$('.'+cid);
+            var input=parent.find('.layui-input');
+            var list=parent.find('dl');
+            var val=list.children('dd:eq(0)').text();
+            input.val("");
+            list.html('<dd lay-value="" class="layui-select-tips">'+val+'</dd>');
+            setHidden("");
+        }
+        function setHidden(val){
+            $('#handler').length<=0 ?
+                $('.layui-form').append('<input type="hidden" id="handler" name="handler" value="'+ val +'" lay-verify="'+element+'"/>'):
+                $('#handler').val(val);
         }
         function siblingDD(element){
             return $("." + element + "").find('dl');
--- js/util.js
+++ js/util.js
@@ -1,8 +1,82 @@
-function setIframeHeight(iframe) {
-	if (iframe) {
-		var iframeWin = iframe.contentWindow || iframe.contentDocument.parentWindow;
-		if (iframeWin.document.body) {
-			iframe.height = iframeWin.document.documentElement.scrollHeight || iframeWin.document.body.scrollHeight;
-		}
-	}
-};
+var zgs_util={
+    dateFormat:function(d){
+        var double=function(num){
+            if(num<9){
+                num="0"+num;
+            }
+            return num;
+        }
+        var date=new Date(d);
+        return date.getFullYear()+'-'+double(date.getMonth()+1)+'-'+double(date.getDate());
+    },
+    getNowFormatDate:function(){
+        var date = new Date();
+        var seperator1 = "-";
+        var seperator2 = ":";
+        var month = date.getMonth() + 1;
+        var strDate = date.getDate();
+        if (month >= 1 && month <= 9) {
+            month = "0" + month;
+        }
+        if (strDate >= 0 && strDate <= 9) {
+            strDate = "0" + strDate;
+        }
+        var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+        return currentdate;
+    },
+    addSuccess:function(result){
+        isSuccess(result,'添加成功')
+    },
+    updSuccess:function(result,callback){
+        if(result.status==1){
+            if(callback && typeof callback=="function"){
+                callback();
+            }
+        }
+        isSuccess(result,'修改成功')
+    },
+    // success:function(result) {
+    //     if(result.status==1){
+    //         window.top.closeAll('iframe');
+    //         layerSucc(result.msg)
+    //     }else{
+    //         layerErr(result.msg)
+    //     }
+    // },
+    error:function(err){
+        layerErr("服务器异常！")
+    },
+    layerErr:function(msg){
+        layerErr(msg)
+    },
+    selectCallback:function(result){
+        if(result.status!=1) {
+            layerErr(result.msg)
+            return false;
+        }
+        return true;
+    },
+    delCallback:function(result){
+        if(result.status==1){
+            layerSucc('删除成功')
+        }else{
+            layerErr(result.msg)
+        }
+    }
+}
+function isSuccess(result,msg) {
+    if(result.status==1){
+        window.top.closeAll('iframe');
+        layerSucc(msg)
+    }else{
+        layerErr(result.msg)
+    }
+}
+function layerErr(msg){
+    window.top.open_layer(msg,{'icon':5,'anim':6})
+}
+function layerSucc(msg){
+    window.top.open_layer(msg,{icon: 1,'time':1000});
+}
